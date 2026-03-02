9705edd729040170981aed359f8598576f3e3721
Subham9628 <subhamkesarwani696@gmail.com>
2026-03-02 16:19:39 +0530

    Initial commit

---
+++ Array/FirstRepeat.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class FirstRepeat {
+
+   // Brute Force O(n²)
+   public int firstRepeatBrute(int[] a) {
+      for (int i = 0; i < a.length; i++) {
+         for (int j = 1 + i; j < a.length; j++) {
+            if (a[i] == a[j]) {
+               return a[i];
+            }
+         }
+      }
+      return -1;
+   }
+   // Optimize solution
+   public int firstRepeatHash(int[] a) {
+      if (a == null || a.length == 1)
+         return -1;
+      HashSet<Integer> set = new HashSet<>();
+      for (int num : set) {
+         if (set.contains(num))
+            return num;
+      }
+      return -1;
+   }
+}
