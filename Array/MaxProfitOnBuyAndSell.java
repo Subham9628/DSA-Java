168af8b73693dace7a89cef9e9acdd93a736a623
Subham9628 <subhamkesarwani696@gmail.com>
2026-03-16 23:51:49 +0530

    Initial commit

---
+++ Array/MaxProfitOnBuyAndSell.java
@@ -0,0 +1,23 @@
+public class MaxProfitOnBuyAndSell {
+    public static int maxProfit(int[] prices) 
+    {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int price:prices)
+        {
+            if(price<min)
+            {
+                min=price;
+            }
+            else
+            {
+                if(max<price-min)
+                {
+                    max=price-min;
+                }
+            }
+        }
+        return max;
+        
+    }
+}
