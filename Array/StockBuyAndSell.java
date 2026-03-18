98354b17b62ba88021a05249ed316288e9c31930
Subham9628 <subhamkesarwani696@gmail.com>
2026-03-18 23:22:43 +0530

    Initial commit

---
+++ Array/StockBuyAndSell.java
@@ -0,0 +1,24 @@
+public class StockBuyAndSell {
+
+    public int maxProfit(int[] prices) 
+    {
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int price:prices)
+        {
+            if(price<min)
+            {
+                min=price;
+            }
+            else
+            {
+                if(max<price-min)
+                {
+                    max=price-min;
+                }
+            }
+        }
+        return max;
+        
+    }
+}
