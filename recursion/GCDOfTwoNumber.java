9fe2511a669cb06c05803c35f6f6b103e3ae1dfd
Subham9628 <subhamkesarwani696@gmail.com>
2026-05-25 15:57:35 +0530

    Initial commit

---
+++ recursion/GCDOfTwoNumber.java
@@ -0,0 +1,16 @@
+public class GCDOfTwoNumber
+{
+    // Euclidean algorithms 
+    public static int gcd(int a, int b) 
+    {
+        if(a%b==0)
+        {
+            return  b;
+        }
+        return gcd(b,a%b);
+    }
+    public static void main(String[] args) 
+    {
+        System.out.println(gcd(4,2));
+    }
+}
